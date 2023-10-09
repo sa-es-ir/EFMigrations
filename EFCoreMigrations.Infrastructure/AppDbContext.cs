e3013661d92fb8f47a22c0f6ae4502d2eeeb62ae
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-10-09 22:16:59 +0700

    add infra

---
+++ EFCoreMigrations.Infrastructure/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFCoreMigrations.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigrations.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+}
